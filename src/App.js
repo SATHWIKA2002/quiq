4c3b0ab9135cbc0f5281cc859b1ac82c6f198fa9
SATHWIKA2002 <98942206+SATHWIKA2002@users.noreply.github.com>
2024-08-13 15:31:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Loginsignup from './components/LoginSignup/Loginsignup';
+
+function App() {
+  return (
+    
+    <div>
+      <Loginsignup/>
+    </div>
+  );
+}
+
+export default App;
